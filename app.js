e81bb8ccfbedff50af23a0c263ebec4345237d70
Moondoobie <sw2345@mac.com>
2016-09-09 22:22:50 -0700

    Modify index.html

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 (function () {
+'use strict';
 
 angular.module('LunchCheck', []);
+.controller('LunchCheckController', LunchCheckController) {
 
-.controller
+    LunchCheckController.$inject = [$scope, $filter];
+    function LunchCheckController($scope, $filter) {
+        $scope.lunch = "";
+        $scope.message = "test";
+
+        $scope.getMessage = function () {
+            $scope.lunch = $scope.lunch.trim();
+            if ($scope.lunch === "") {
+                $scope.message = "Please enter data first";
+            }
+
+            var lunchCount = 1;
+            for (var i = 0; i < $scope.lunch.length; i++) {
+                if ($scope.lunch[i] === ',') {
+                    lunchCount++;
+                }
+            }
+
+
+            if (lunchCount < 4) {
+                $scope.message = "Enjoy!"
+            } else {
+                $scope.message = "Too much!";
+            }
+    }
+};
+
+
+});
 
 })();
